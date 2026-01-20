5dc228b23ca85e8aa2ee7169c78c07b2dfc90ba2
csibokk <42963413+csibokk@users.noreply.github.com>
2026-01-20 16:18:41 +0800

    Update make_research.R

--- docs/make_research.R
+++ docs/make_research.R
@@ -1,5 +1,5 @@
 rmarkdown::render("docs/svm-research-statement.Rmd", 
-                  output_file="1.pdf", 
+                  output_file="svm-research-statement.pdf", 
                   params=list(github="svmiller"),
                   rmarkdown::pdf_document(template = stevetemplates::templ_statement(), 
                                           latex_engine = "xelatex", dev="cairo_pdf"))
